37ba9aa4f9579565da2f58866191bad0e6bdc161
Bruno Mossmann <41453787+brunomossmann007@users.noreply.github.com>
2021-09-22 14:30:06 -0300

    Jogos

---
+++ Python.py
@@ -0,0 +1,27 @@
+imprimir = print()
+toda função vai o ()
+\n = nova linha
+sep="-" separar palavras em um print
+end=(\n) = nova linha em um print
+
+
+Declaração de variavel:
+exemplo:
+pais = "Italia"
+type(pais)
+string
+
+quantidade = 4
+type(quantidade)
+int
+
+imprimindo varivavel
+print(pais, "ganhou", quantidade, "titulos mundiais")
+Italia ganhou 4 titulos mundiais
+
+
+entrada do teclado:
+input
+exemplo:
+chute = input("digite o numero desejado:", chute)
+
